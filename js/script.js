3f2a028e21b5bfde53552b87e8a3d69843c6de9c
Sapik <Sapik_19@mail.ru>
2022-06-15 16:07:30 +0300

    Object test

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 "use strict";
 
+const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const youLike = confirm('Желаете пройти опрос?');
+if (youLike == true) {
+    const personaMovieDB = {
+        count: numberOfFilm,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+    
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', ''),
+        c = prompt('Какой кинотеатр выбрали?', ''),
+        d = prompt('В каком зале сидели', '');
+    
+    personaMovieDB.movies[a] = b;
+    personaMovieDB.actors[c] = d;
+
+    alert('Спасибо за пройденный опрос!');
+
+    console.log(personaMovieDB);
+} else {
+    alert('Вы уверены?');
+}
